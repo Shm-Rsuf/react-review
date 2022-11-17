3a5d760b262eb2015aa9992c62fc44e655ffe6a8
shm_usuf <shmusuf@gmail.com>
2022-11-17 07:27:40 +0600

    todo added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Form from "./components/Form";
+import Todo from "./components/Todo";
 
 export default function App() {
   return (
     <div>
-      <Form />
+      <Todo />
     </div>
   );
 }
