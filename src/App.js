2321d52ff5c61fdbdc731003eded847d9f325e80
shm_usuf <shmusuf@gmail.com>
2022-11-17 09:29:36 +0600

    form added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Todo from "./components/Todo";
+import Form from "./components/Form";
 
 export default function App() {
   return (
     <div>
-      <Todo />
+      <Form />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Todo() {
-  return <div>Todo</div>;
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const inputHandler = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const todoListHandler = (event) => {
+    event.preventDefault();
+
+    let tempList = todoList;
+    tempList.push(todo);
+    setTodoList(tempList);
+  };
+
+  return (
+    <div>
+      <form onSubmit={todoListHandler}>
+        <input type="text" value={todo} onChange={inputHandler} />
+        <button type="submit">add</button>
+      </form>
+    </div>
+  );
 }
